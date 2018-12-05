24251e68f0148446cf8ce82826f112c8d4f8acab
Joey Heffner <joseph_heffner@brown.edu>
2018-12-05 14:30:19 -0500

    Adding get_openfec function

---
+++ fecScrape/R/get_openfec.R
@@ -0,0 +1,25 @@
+#' Make a request to the OpenFEC API
+#'
+#' This function constructs the correct URL to send a GET request to the OpenFEC API. Returns the results as a list including both raw and parsed JSON information. OpenFEC query parameters are documented here https://api.open.fec.gov/developers/.
+#' @param path A character string of the OpenFEC endpoint.
+#' @param query_parameters A list with values of the parameters to use in the request.
+#'
+#' @export 
+
+get_openfec <- function(path, query_parameters = list()) {
+  
+  #Construct URL to scrape from
+  url <- httr::modify_url('https://api.open.fec.gov/', path = paste0("/v1",path), query = query_parameters)
+  
+  response <- httr::GET(url)
+
+  parsed_response <- jsonlite::fromJSON(httr::content(response, "text", encoding="UTF-8"), simplifyVector = FALSE)
+  
+  all <- list(
+    raw = response,
+    parsed = parsed_response
+  )
+  
+  return(all) # returns a list including both the raw JSON and the parsed JSON
+  
+}
